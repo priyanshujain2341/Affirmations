7cc5cfe8d30f47c351c36f1bbd459aa080bccb67
Priyanshu Jain <priyanshujain2341@gmail.com>
2022-01-12 20:16:37 +0530

    Tests lists and adapters

--- app/src/test/java/com/polymath/affirmations/ExampleUnitTest.kt
+++ app/src/test/java/com/polymath/affirmations/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.polymath.affirmations
 
+import android.content.Context
+import com.polymath.affirmations.adapter.ItemAdapter
+import com.polymath.affirmations.model.Affirmation
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.mockito.Mockito.mock
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,6 +13,23 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    private val context = mock(Context::class.java)
+
+    fun adapter_size()
+    {
+        val data = listOf(
+            Affirmation(R.string.affirmation1, R.drawable.image1),
+            Affirmation(R.string.affirmation2, R.drawable.image2)
+        )
+        val adapter = ItemAdapter(context, data)
+
+        /**
+         * Return the size of your dataset (invoked by the layout manager)
+         */
+
+        assertEquals("ItemAdapter is not the correct size", data.size, adapter.itemCount)
+    }
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
